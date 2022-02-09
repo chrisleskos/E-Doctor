60f9580d24e4f8f4bc5c95aaf02ae3f6f361f13d
PCined <chrisles143@gmail.com>
2022-02-09 03:10:03 +0200

    Styling

--- E-Doctor1/Controllers/HomeController.cs
+++ E-Doctor1/Controllers/HomeController.cs
@@ -80,19 +80,5 @@ namespace E_Doctor1.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
